cef3f948204ba03e6448976d64d047ef4cb2f4bf
Sandro Gauci <sandro@enablesecurity.com>
2014-11-07 18:05:30 +0000

    added some new barracuda checks
    now looking for BNI_persistence and BNES_ in cookies for barracuda

--- wafw00f/__init__.py
+++ wafw00f/__init__.py
@@ -399,6 +399,11 @@ class WafW00F(waftoolsengine):
         # credit goes to Charlie Campbell
         if self.matchcookie('^BNI__BARRACUDA_LB_COOKIE='):
             return True
+        # credit goes to yours truly      
+        if self.matchcookie('^BNI_persistence='):
+            return True
+        if self.matchcookie('^BN[IE]S_.*?='):
+            return True
         return False
 
     def isdenyall(self):
