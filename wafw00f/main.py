02f46606df7f22e61b2fc30ab9936731ae28b8c4
akun <6awkun@gmail.com>
2015-04-17 13:56:04 +0800

    add plugin: Microsoft URLScan

--- wafw00f/main.py
+++ wafw00f/main.py
@@ -496,26 +496,6 @@ class WafW00F(waftoolsengine):
             return True
         return False
 
-    def isurlscan(self):
-        detected = False
-        testheaders = dict()
-        testheaders['Translate'] = 'z' * 10
-        testheaders['If'] = 'z' * 10
-        testheaders['Lock-Token'] = 'z' * 10
-        testheaders['Transfer-Encoding'] = 'z' * 10
-        r = self.normalrequest()
-        if r is None:
-            return
-        response, _tmp = r
-        r = self.normalrequest(headers=testheaders)
-        if r is None:
-            return
-        response2, _tmp = r
-        if response.status != response2.status:
-            if response2.status == 404:
-                detected = True
-        return detected
-
     def ismodsecuritypositive(self):
         detected = False
         self.normalrequest(usecache=False, cacheresponse=False)
@@ -555,7 +535,6 @@ class WafW00F(waftoolsengine):
     wafdetections['Citrix NetScaler'] = isnetscaler
     # lil bit more complex
     wafdetections['Aqtronix WebKnight'] = iswebknight
-    wafdetections['Microsoft URLScan'] = isurlscan
     wafdetections['eEye Digital Security SecureIIS'] = issecureiis
     #wafdetections['BeeWare'] = isbeeware
     #wafdetections['ModSecurity (positive model)'] = ismodsecuritypositive removed for now
