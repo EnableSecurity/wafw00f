8b6429173e177acf3b1303e71ef717a2f26d950b
Kevin Kirsche <Kev.Kirsche+GitHub@gmail.com>
2017-08-15 14:03:09 -0400

    Add classifiers and Keywords per PyPi sample
    https://github.com/pypa/sampleproject/blob/master/setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,17 @@ setup(
         'beautifulsoup4==4.6.0',
         'pluginbase==0.3',
     ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'Topic :: Internet',
+        'Topic :: Security',
+        'Topic :: System :: Networking :: Firewalls',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+    ],
+    keywords='waf firewall detector',
     extras_require={
         'dev': [
             'prospector==0.10.1',
