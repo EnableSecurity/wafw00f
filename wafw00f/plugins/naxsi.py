2c09d4c8bf9fea51f4179a56471a61c94c5e5612
Sandro Gauci <sandro@enablesecurity.com>
2017-12-29 10:34:28 +0100

    simpler is better

--- wafw00f/plugins/naxsi.py
+++ wafw00f/plugins/naxsi.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
-
 NAME = 'Naxsi'
-X_DATA_ORIGIN = 'X-Data-Origin'
 
 def is_waf(self):
-    if self.matchheader(('X-Data-Origin', 'naxsi/waf'), attack=False, ignorecase=True):
-        return True
+    return self.matchheader(('X-Data-Origin', '^naxsi'))
