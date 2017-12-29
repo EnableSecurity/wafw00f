1c038d6c79b7da9149af42b76377a5c25d4f1551
Sandro Gauci <sandro@enablesecurity.com>
2017-12-29 10:48:35 +0100

    Update wallarm.py

--- wafw00f/plugins/wallarm.py
+++ wafw00f/plugins/wallarm.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
-
 NAME = 'Wallarm'
 
-
 def is_waf(self):
-    if self.matchheader(('server', "nginx-wallarm")):
-        return True
+    return self.matchheader(('server', "nginx-wallarm"))
+
