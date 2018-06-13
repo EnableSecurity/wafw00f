0eaffffc0334bde0c6ff10cb04beb9b3e8946287
Fabian Affolter <fabian@affolter-engineering.ch>
2018-06-14 00:39:07 +0200

    Update install_requires

--- setup.py
+++ setup.py
@@ -17,7 +17,8 @@ setup(
     scripts=['wafw00f/bin/wafw00f'],
     install_requires=[
         'beautifulsoup4==4.6.0',
-        'pluginbase==0.3',
+        'pluginbase==0.7',
+        'html5lib==1.0.1'
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -28,6 +29,7 @@ setup(
         'Topic :: System :: Networking :: Firewalls',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
     ],
     keywords='waf firewall detector fingerprint',
     extras_require={
