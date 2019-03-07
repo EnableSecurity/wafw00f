bac59a6a5aadaa057855c2d28ab410f88fc85ba0
Hamza Zerhouni <hamza.zerhouni@ozon.io>
2019-03-07 15:47:00 +0100

    Detect presence of Wordfence

---
+++ wafw00f/plugins/wordfence.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+
+NAME = 'Wordfence'
+
+
+def is_waf(self):
+    r = self.normalrequest()
+
+    if r is None:
+        return False
+
+    normalresponse, _ = r
+
+    link_header = normalresponse.getheader('Link') or ""
+
+    if "https://api.w.org/" not in link_header:
+        # Does not appear to be a wordpress at all
+        return False
+
+    r = self.request("GET", self.path + "wp-content/plugins/wordfence/")
+
+    if r is None:
+        return False
+
+    pluginresponse, _ = r
+
+    return pluginresponse.status == 200
