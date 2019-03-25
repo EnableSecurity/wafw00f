e63e25f9fa3e25c85279a0e27fef69db6dea4835
g0tmi1k <have.you.g0tmi1k@gmail.com>
2019-03-25 11:25:40 +0800

    Add another check for Sucuri

--- wafw00f/plugins/sucuri.py
+++ wafw00f/plugins/sucuri.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 
+"""
+Homepage: https://sucuri.net
+"""
 NAME = 'Sucuri WAF'
 
 def is_waf(self):
+    """
+    Checks for server headers containing "Sucuri"
+    Example: `Server: Sucuri/Cloudproxy` (Happens on non HTTP 200 requests)
+    """
+    if self.matchheader(('server', 'Sucuri'))
+      return True
+
+    """
+    Checks for headers containing "X-Sucuri-ID"
+    Example: `x-sucuri-id: 13008` (case depends on HTTP status)
+    """
     return self.matchheader(('X-Sucuri-ID', '.+'))
