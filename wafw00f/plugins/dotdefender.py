f03836d48e7adb015b38f5bf7389a1d4b54e0a18
Sandro Gauci <sandro@enablesecurity.com>
2019-03-26 16:53:57 +0100

    retaining header check because it is inexpensive worked for old versions

--- wafw00f/plugins/dotdefender.py
+++ wafw00f/plugins/dotdefender.py
@@ -5,16 +5,15 @@ NAME = 'Applicure dotDefender'
 
 
 def is_waf(self):
-    detected = False
     for attack in self.attacks:
         r = attack(self)
         if r is None:
             return
         response, responsepage = r
+        if response.getheader('X-dotDefender-denied'):
+            return True
         # Using a bytes like object directly for comparison resolved 
         # the load of decoding it again.
         if b'dotDefender Blocked Your Request' in responsepage:
-            detected = True
-            break
-    return detected
-    return self.matchheader(['X-dotDefender-denied', '^1$'], attack=True)
+            return True
+    return False
