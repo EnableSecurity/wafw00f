dfb95f43d588711572c70b689e2bd0e36b3e173a
0xInfection <theinfecteddrake@gmail.com>
2019-03-26 13:38:55 +0530

    Added correct fingerprint for dotdefender

--- wafw00f/plugins/dotdefender.py
+++ wafw00f/plugins/dotdefender.py
@@ -5,5 +5,16 @@ NAME = 'Applicure dotDefender'
 
 
 def is_waf(self):
-    # thanks to j0e
+    detected = False
+    for attack in self.attacks:
+        r = attack(self)
+        if r is None:
+            return
+        response, responsepage = r
+        # Using a bytes like object directly for comparison resolved 
+        # the load of decoding it again.
+        if b'dotDefender Blocked Your Request' in responsepage:
+            detected = True
+            break
+    return detected
     return self.matchheader(['X-dotDefender-denied', '^1$'], attack=True)
