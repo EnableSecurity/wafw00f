20d4283fcc2241ef792e30bc8ea2ea979be3d313
0xInfection <theinfecteddrake@gmail.com>
2019-03-28 14:38:42 +0530

    Added new fingerprints for incapsula

--- wafw00f/plugins/incapsula.py
+++ wafw00f/plugins/incapsula.py
@@ -5,9 +5,15 @@ NAME = 'Incapsula WAF'
 
 
 def is_waf(self):
-    # credit goes to Charlie Campbell
-    if self.matchcookie('^.incap_ses'):
+    if self.matchcookie('^incap_ses.*='):
         return True
-    if self.matchcookie('^visid.*='):
+    if self.matchcookie('^visid_incap.*='):
         return True
+    for attack in self.attacks:
+        r = attack(self)
+        if r is None:
+            return
+        _, responsepage = r
+        if any(a in responsepage for a in (b"Incapsula incident ID:", b"/_Incapsula_Resource")):
+            return True
     return False
