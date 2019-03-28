ede87bc522f026c8e5c393a5ceda8d0e3f556660
0xInfection <theinfecteddrake@gmail.com>
2019-03-28 14:41:32 +0530

    Added new fingerprints for netscaler

--- wafw00f/plugins/netscaler.py
+++ wafw00f/plugins/netscaler.py
@@ -10,19 +10,28 @@ def is_waf(self):
     if not it will try to find if a "Cneonction" or "nnCoection" is returned
     for any of the attacks sent
     """
-    # NSC_ and citrix_ns_id come from David S. Langlands <dsl 'at' surfstar.com>
+    # Cookies are set only when someone is authenticated.
+    # Not much reliable since wafw00f isn't authenticating.
     if self.matchcookie('^(ns_af=|citrix_ns_id|NSC_)'):
         return True
-    if self.matchheader(('Cneonction', 'close'), attack=True):
+    # Not quite sure about this, may return false positives, not
+    # one of the sites I've met returned this header
+    if self.matchheader(('Location', '\\/vpn\\/index\\.html')):
         return True
-    if self.matchheader(('nnCoection', 'close'), attack=True):
-        return True
-    if self.matchheader(('Via', 'NS-CACHE'), attack=True):
-        return True
-    if self.matchheader(('x-client-ip', '.'), attack=True):
-        return True
-    if self.matchheader(('Location', '\/vpn\/index\.html')):
-        return True
-    if self.matchcookie('^pwcount'):
-        return True
+    # The actual fngerprints are obtained upon attack in source.
+    for attack in self.attacks:
+        r = attack(self)
+        if r is None:
+            return
+        response, responsepage = r
+        if any(a in responsepage for a in (b"NS Transaction ID:", b"AppFW Session ID:",
+            b'Violation Category: APPFW_', b'Citrix|NetScaler')):
+            return True
+        # Reliable but not all servers return this header
+        if self.matchheader(('Via', 'NS-CACHE')):
+            return True
+        if self.matchheader(('Cneonction', 'close')):
+            return True
+        if self.matchheader(('nnCoection', 'close')):
+            return True
     return False
