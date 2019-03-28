e9b471869a4649a3ab5c942ce995951da6a72ef1
0xInfection <theinfecteddrake@gmail.com>
2019-03-28 10:54:22 +0530

    Added new securesphere fingerprint

--- wafw00f/plugins/imperva.py
+++ wafw00f/plugins/imperva.py
@@ -5,13 +5,17 @@ NAME = 'Imperva SecureSphere'
 
 
 def is_waf(self):
-    # thanks to Mathieu Dessus <mathieu.dessus(a)verizonbusiness.com> for this
-    # might lead to false positives so please report back to sandro@enablesecurity.com
     for attack in self.attacks:
         r = attack(self)
         if r is None:
             return
-        response, _ = r
+        response, page = r
+        # this is more reliable fingerprint, the standard error page
+        if all(m in page for m in (b'<H2>Error</H2>', b'<title>Error</title>', b'The incident ID is:',
+             b"This page can't be displayed.", b"Contact support for additional information.")):
+            return True
+        # never met a site returning this, but still considered keeping it
+        # might lead to false positives
         if response.version == 10:
             return True
     return False
