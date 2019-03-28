917a064ef7e5f6db17aaee6a0f62052a8eadfcf9
0xInfection <theinfecteddrake@gmail.com>
2019-03-28 10:43:04 +0530

    Added new airlock fingerprints

--- wafw00f/plugins/airlock.py
+++ wafw00f/plugins/airlock.py
@@ -5,5 +5,18 @@ NAME = 'Ergon Airlock'
 
 
 def is_waf(self):
-    # credit goes to W3AF
+	# This method of detection is old (though most reliable), 
+    # so we check it first
     return self.matchcookie('^AL[_-]?(SESS|LB)=')
+    # Nowadays many sites running Airlock do not set cookies 
+    # directly without authentication. So we have to make the 
+    # last final check of the characteristic block page.
+    for attack in self.attacks:
+        r = attack(self)
+        if r is None:
+            return
+        _, responsepage = r
+        if all(a in responsepage for a in (b"Check your request and all parameters", 
+        	b"Bad Request", b"The server detected a syntax error in your request")):
+            return True
+    return False
