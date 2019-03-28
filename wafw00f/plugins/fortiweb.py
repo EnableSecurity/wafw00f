7a9f32a1e5a21cea5d11cc3ef93e2e163588f9e1
0xInfection <theinfecteddrake@gmail.com>
2019-03-28 10:52:08 +0530

    Added new fortiweb fingerprint

--- wafw00f/plugins/fortiweb.py
+++ wafw00f/plugins/fortiweb.py
@@ -3,4 +3,14 @@
 NAME = 'FortiWeb'
 
 def is_waf(self):
-    return self.matchcookie('FORTIWAFSID=')
+    if self.matchcookie('FORTIWAFSID='):
+        return True
+    for attack in self.attacks:
+        r = attack(self)
+        if r is None:
+            return
+        _, responsepage = r
+        # Faced false positives hence reverting back to unique ones only
+        if all(m in responsepage for m in (b'.fgd_icon', b'Client IP')):
+            return True
+    return False
