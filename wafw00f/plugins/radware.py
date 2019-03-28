75165007a82b273dbe3814d1b6a4a804ecd975d3
0xInfection <theinfecteddrake@gmail.com>
2019-03-28 14:42:33 +0530

    Added new fingerprints for radware

--- wafw00f/plugins/radware.py
+++ wafw00f/plugins/radware.py
@@ -3,4 +3,15 @@
 NAME = 'Radware AppWall'
 
 def is_waf(self):
-    return self.matchheader(('X-SL-CompState', '.'))
+    if self.matchheader(('X-SL-CompState', '.')):
+        return True
+    for attack in self.attacks:
+        r = attack(self)
+        if r is None:
+            return
+        _, responsebody = r
+        # Most reliable fingerprint is this on block page
+        if all(i in responsebody for i in (b'because we have detected unauthorized activity', 
+            b'<TITLE>Unauthorized Request Blocked</TITLE>', b'mailto:CloudWebSec@radware.com')):
+            return True
+    return False
