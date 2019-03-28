c029d58a7c842f82963d151dc57bd04fb69cbeb2
0xInfection <theinfecteddrake@gmail.com>
2019-03-28 14:42:54 +0530

    Added new fingerprints for safedog

--- wafw00f/plugins/safedog.py
+++ wafw00f/plugins/safedog.py
@@ -9,6 +9,13 @@ def is_waf(self):
         return True
     if self.matchheader(('server', '^Safedog')):
         return True
-    if self.matchheader(('x-powered-by', '^WAF/\d\.\d')):
-        return True
+    for attack in self.attacks:
+        r = attack(self)
+        if r is None:
+            return
+        _, responsebody = r
+        # Most reliable fingerprint is this on block page
+        if any(i in responsebody for i in (b'safedogsite/broswer_logo.jpg', 
+            b'404.safedog.cn/sitedog_stat.html', b'404.safedog.cn/images/safedogsite/head.png')):
+            return True
     return False
