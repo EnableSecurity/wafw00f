1db4f2e2687740cbc3d6dfd64b0ec6c530178d70
0xInfection <theinfecteddrake@gmail.com>
2019-03-28 14:39:48 +0530

    Added new fingerprints for naxsi

--- wafw00f/plugins/naxsi.py
+++ wafw00f/plugins/naxsi.py
@@ -3,4 +3,17 @@
 NAME = 'Naxsi'
 
 def is_waf(self):
-    return self.matchheader(('X-Data-Origin', '^naxsi'))
+	# Sometimes naxsi waf returns 'x-data-origin: naxsi/waf'
+    if self.matchheader(('X-Data-Origin', '^naxsi(.*)?')):
+    	return True
+    # Found samples returning 'server: naxsi/2.0'
+    if self.matchheader(('server', 'naxsi(.*)?')):
+    	return True
+    for attack in self.attacks:
+        r = attack(self)
+        if r is None:
+            return
+        _, responsebody = r
+        if any(i in responsebody for i in (b'Blocked By NAXSI', b'Naxsi Blocked Information')):
+            return True
+    return False
