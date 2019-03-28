3663f969ed872ac964d79a94207ca8274c184c5e
0xInfection <theinfecteddrake@gmail.com>
2019-03-28 10:47:09 +0530

    Added new barracuda fingerprint

--- wafw00f/plugins/barracuda.py
+++ wafw00f/plugins/barracuda.py
@@ -5,15 +5,21 @@ NAME = 'Barracuda Application Firewall'
 
 
 def is_waf(self):
-    # credit goes to W3AF
     if self.matchcookie('^barra_counter_session='):
         return True
-    # credit goes to Charlie Campbell
     if self.matchcookie('^BNI__BARRACUDA_LB_COOKIE='):
         return True
-    # credit goes to yours truly
     if self.matchcookie('^BNI_persistence='):
         return True
     if self.matchcookie('^BN[IE]S_.*?='):
         return True
+    for attack in self.attacks:
+        r = attack(self)
+        if r is None:
+            return
+        _, responsepage = r
+        # At the bottom of blockpage there is a copyright
+        # notice indicating barracuda networks.
+        if b'Barracuda Networks, Inc' in responsepage:
+            return True
     return False
