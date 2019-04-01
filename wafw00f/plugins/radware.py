a81455861f6f68bf4c21b4c3c4388771d2abac5c
0xInfection <theinfecteddrake@gmail.com>
2019-04-01 07:56:52 +0530

    Restored a radware fingerprint

--- wafw00f/plugins/radware.py
+++ wafw00f/plugins/radware.py
@@ -15,4 +15,7 @@ def is_waf(self):
             b'<TITLE>Unauthorized Request Blocked</TITLE>', b'If you believe that there has been some mistake',
             b'?Subject=Security Page - Case Number')):
             return True
+        # Restored a fingerprint for radware previously discarded
+        if b'CloudWebSec@radware.com' in responsebody:
+            return True
     return False
