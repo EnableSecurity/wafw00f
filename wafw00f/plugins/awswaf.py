26cef560402dbb1c6544d6689f4616eae0e53e27
0xInfection <theinfecteddrake@gmail.com>
2019-04-06 12:28:35 +0530

    Added some more fingerprints for Amamzon AWS

--- wafw00f/plugins/awswaf.py
+++ wafw00f/plugins/awswaf.py
@@ -3,6 +3,12 @@
 NAME = 'AWS WAF'
 
 def is_waf(self):
+    # Prioritize these checks first which doesn't require attack
+    if self.matchheader(('X-AMZ-ID', '.*')):
+        return True
+    if self.matchheader(('X-AMZ-Request-ID', '.*')):
+        return True
+    # Move to attack phase for identification
     if self.matchheader(('Server', 'awselb/2\\.0'), attack=True):
         return True
     return False
