735f5d330bfbaf163008413cc812ec802d1c8add
0xInfection <theinfecteddrake@gmail.com>
2019-04-06 20:47:56 +0530

    Added bekchy waf fingerprint

---
+++ wafw00f/plugins/bekchy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+NAME = 'Faydata Bekchy'
+
+
+def is_waf(self):
+    for attack in self.attacks:
+        r = attack(self)
+        if r is None:
+            return
+        _, page = r
+        # Both signatures are contained within response, so checking for any one of them
+        if any(i in page for i in (b'Bekchy - Access Denied', b'https://bekchy.com/report')):
+            return True
+    return False
