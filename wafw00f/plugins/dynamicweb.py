02e021f317c4365cc2ec9d800c55b0eb1cae2c79
0xInfection <theinfecteddrake@gmail.com>
2019-04-07 18:19:50 +0530

    Added dynamic web injection check fingerprint

---
+++ wafw00f/plugins/dynamicweb.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+
+NAME = 'DynamicWeb Injection Check'
+
+
+def is_waf(self):
+    if self.matchcookie('crawlprotecttag'):
+        return True
+    for attack in self.attacks:
+        r = attack(self)
+        if r is None:
+            return
+        response, _ = r
+        if response.getheader('X-403-Status-By') and response.status == 403:
+            return True
+    return False
