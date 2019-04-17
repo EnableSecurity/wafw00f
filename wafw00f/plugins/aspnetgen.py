15776b49ae826da3f81c8416df97d4e5f843e8c3
0xInfection <theinfecteddrake@gmail.com>
2019-04-17 19:22:21 +0530

    Added more checks for accurate detection

--- wafw00f/plugins/aspnetgen.py
+++ wafw00f/plugins/aspnetgen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 
-NAME = 'ASP.NET Generic'
+NAME = 'ASP.NET Generic Web Application Protection'
 
 
 def is_waf(self):
@@ -19,7 +19,7 @@ def is_waf(self):
         if r is None:
             return
         _, page = r
-        if b'This generic 403 error means that the authenticated user is not authorized to use the requested resource' in page:
+        if any(i in page for i in (b'This generic 403 error means that the authenticated user is not authorized to use the requested resource', b'request could not be understood', b'potentially dangerous request querystring')):
             detected2 = True
     # We return whatever we got from those 3 checks
     if detected1 and detected2:
