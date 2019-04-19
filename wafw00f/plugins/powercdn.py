c60db72f6ef27a69d2891433aa578e7763a2496b
0xInfection <theinfecteddrake@gmail.com>
2019-04-19 16:49:42 +0530

    Added update to new powercdn fingerprints

--- wafw00f/plugins/powercdn.py
+++ wafw00f/plugins/powercdn.py
@@ -5,6 +5,16 @@ NAME = 'PowerCDN'
 
 
 def is_waf(self):
+    # More later versions of PowerCDN waf >= v4.x return these two headers specifically.
+    # X-Cache: HIT from cc89.powercdn.com
+    # Via: 1.1 cc89.powercdn.com (PowerCDN/4.1)
+    # I implemented them in this way:
+    if self.matchheader(('Via', '(.*)?powercdn.com(.*)?')):
+        return True
+    if self.matchheader(('X-Cache', '(.*)?powercdn.com(.*)?')):
+        return True
+    # This used be found on earlier versions of PowerCDN no longer now. 
     if self.matchheader(('PowerCDN', '.+')):
         return True
+    
     return False
