2ba7255548cb57ddcb4ea8f5504da4a92bc86fa6
0xInfection <theinfecteddrake@gmail.com>
2019-04-19 18:32:27 +0530

    Added new expression engine waf fingerprint

---
+++ wafw00f/plugins/expressionengine.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+
+NAME = 'Expression Engine (EllisLab)'
+
+
+def is_waf(self):
+    for attack in self.attacks:
+        r = attack(self)
+        if r is None:
+            return
+        response, page = r
+        # There are traces found where cookie is returning values like:
+        # Set-Cookie: exp_last_query=834y8d73y94d8g983u4shn8u4shr3uh3
+        # Set-Cookie: exp_last_id=b342b432b1a876r8
+        if 'exp_last_' in response.getheader('Set-Cookie'):
+            return True
+        # In-page fingerprints vary a lot in different sites. Hence these are not quite reliable.
+        if any(i in page for i in (b'Invalid GET Data', 'Invalid URI')):
+            return True
+    return False
