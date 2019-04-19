8ed55d606837d039018f5e59d6a118846e29fa8c
0xInfection <theinfecteddrake@gmail.com>
2019-04-19 18:34:18 +0530

    Improvised f5 ASM fingerprints

--- wafw00f/plugins/f5bigipasm.py
+++ wafw00f/plugins/f5bigipasm.py
@@ -5,6 +5,12 @@ NAME = 'BIG-IP Application Security Manager (F5 Networks)'
 
 
 def is_waf(self):
-    if self.matchcookie(r'^TS[a-zA-Z0-9]{3,8}='):
-        return True
+    # Actual fingerprint is this arising due to attack strings
+    for attack in self.attacks:
+        r = attack(self)
+        if r is None:
+            return
+        _, page = r
+        if b'The requested URL was rejected. Please consult with your administrator' in page:
+            return True
     return False
