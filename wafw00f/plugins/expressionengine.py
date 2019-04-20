b1b8c9b4e392d4865756ece6528e6668e2bc8975
0xInfection <theinfecteddrake@gmail.com>
2019-04-20 16:30:45 +0530

    Fix to avoid NoneType bugs

--- wafw00f/plugins/expressionengine.py
+++ wafw00f/plugins/expressionengine.py
@@ -13,8 +13,9 @@ def is_waf(self):
         # There are traces found where cookie is returning values like:
         # Set-Cookie: exp_last_query=834y8d73y94d8g983u4shn8u4shr3uh3
         # Set-Cookie: exp_last_id=b342b432b1a876r8
-        if 'exp_last_' in response.getheader('Set-Cookie'):
-            return True
+        if response.getheader('Set-Cookie'):
+            if 'exp_last_' in response.getheader('Set-Cookie'):
+                return True
         # In-page fingerprints vary a lot in different sites. Hence these are not quite reliable.
         if any(i in page for i in (b'Invalid GET Data', b'Invalid URI')):
             return True
