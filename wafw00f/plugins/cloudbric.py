c45d6a7563f254eb6e1367a9d0959e47db7596e1
Infected Drake <theinfecteddrake@gmail.com>
2019-05-11 11:27:54 +0530

    Fixed some bugs + More Cloudbric Fingerprints

--- wafw00f/plugins/cloudbric.py
+++ wafw00f/plugins/cloudbric.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 
 
-NAME = 'Cloudbric (Zendesk)'
+NAME = 'Cloudbric (Penta Security)'
 
 
 def is_waf(self):
-    # Approach reveals itself within the server headers without any mal requests
-    if self.matchheader(('Server', 'Approach Web Application Firewall')):
-        return True
     for attack in self.attacks:
         r = attack(self)
         if r is None:
             return
         _, page = r
         # Cloudbric has nice characteristic blockpage, easy to detect.
-        if any(i in page for i in (b'<title>Cloudbric | ERROR!', b'Your request was blocked by Cloudbric',
-            b'please contact Cloudbric Support.', b'https://cloudbric.zendesk.com', b'Cloudbric Help Center')):
+        # Obtained a new variant of the blockpage offered by CloudBric. Diff. attack methods result in diff fingerprints
+        if any(i in page for i in (b'<title>Cloudbric | ERROR!', b'Your request was blocked by Cloudbric', b'<title>ERROR! |',
+            b'please contact Cloudbric Support.', b'https://cloudbric.zendesk.com', b'Cloudbric Help Center', 
+            b'malformed request syntax, invalid request message framing, or deceptive request routing.')):
             return True
     return False
