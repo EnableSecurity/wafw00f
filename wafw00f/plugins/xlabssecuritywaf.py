45d08103b3cbc53f9a01edcf371acf2cabdf04fd
Infected Drake <theinfecteddrake@gmail.com>
2019-05-18 18:33:53 +0530

    Added another header fingerprint for XLabs

--- wafw00f/plugins/xlabssecuritywaf.py
+++ wafw00f/plugins/xlabssecuritywaf.py
@@ -5,8 +5,12 @@ NAME = 'XLabs Security WAF (XLabs)'
 def is_waf(self):
     if self.matchheader(('x-cdn', 'XLabs Security')):
         return True
-    # Another nice fingerprint found where server returns a
+    # This is another fingerprint header found in a different site
+    # during extensive testing for this plugin.
+    if self.matchheader(('Secured', r'^By XLabs Security.+?')):
+        return True
+    # Another nice fingerprint found where server returns attack
     # header as 'Server: XLabs WAF v3.0 http://www.xlabs.com.br/waf'
-    if self.matchheader(('server', r'XLabs WAF(.*)?')):
+    if self.matchheader(('Server', r'^XLabs WAF.+?'), attack=True):
         return True
     return False
