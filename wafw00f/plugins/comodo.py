78a80e56ccd68332d4bbaed5760b91ff6a95e189
0xInfection <theinfecteddrake@gmail.com>
2019-06-05 13:16:15 +0530

    Added name fix for comodo waf

--- wafw00f/plugins/comodo.py
+++ wafw00f/plugins/comodo.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-NAME = 'Comodo (Comodo CyberSecurity Solutions)'
+NAME = 'Comodo cWatch (Comodo CyberSecurity)'
 
 def is_waf(self):
     # The (.*)? was required since some servers return additional info like
     # version of the WAF. eg. Protected by COMODO WAF mod_fcgid/2.3.9   
-    if self.matchheader(('server', r'Protected by COMODO WAF(.*)?')):
+    if self.matchheader(('Server', r'Protected by COMODO WAF(.+)?')):
         return True
     return False
