43ebd1d3210df2e59e895cf2b1f9c613defcc530
0xInfection <theinfecteddrake@gmail.com>
2019-06-05 14:27:46 +0530

    New fingerprint for AWS

--- wafw00f/plugins/awswaf.py
+++ wafw00f/plugins/awswaf.py
@@ -10,6 +10,8 @@ def is_waf(self):
         return True
     if self.matchheader(('X-AMZ-Request-ID', '.*')):
         return True
+    if self.matchcookie(r'^aws.?alb='):
+        return True
     # Move to attack phase for identification
     if self.matchheader(('Server', r'awselb/2\.0'), attack=True):
         return True
