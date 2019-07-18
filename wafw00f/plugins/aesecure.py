fe9f765681c602dbc3c443f83f80a5fcc9a98cbd
0xInfection <theinfecteddrake@gmail.com>
2019-07-18 20:14:02 +0530

    Adding the airlock plugin

--- wafw00f/plugins/aesecure.py
+++ wafw00f/plugins/aesecure.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'aeSecure (aeSecure)'
 
 
 def is_waf(self):
     schemes = [
-        self.matchHeader(('aeSecure-code', '.+')),
+        self.matchHeader(('aeSecure-code', '.+?')),
         self.matchContent(r'aesecure_denied.png')
     ]
     if any(i for i in schemes):
