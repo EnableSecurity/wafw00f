1e3b5fd952f360005f22f77f2dbfbcd61bb44fb3
0xInfection <theinfecteddrake@gmail.com>
2019-07-18 20:15:51 +0530

    Added anquanbao plugin

--- wafw00f/plugins/anquanbao.py
+++ wafw00f/plugins/anquanbao.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'Anquanbao (Anquanbao)'
 
 
 def is_waf(self):
-    if self.matchheader(('X-Powered-By-Anquanbao', '.+')):
+    schemes = [
+        self.matchHeader(('X-Powered-By-Anquanbao', '.+?')),
+        self.matchContent(r'aqb_cc/error/')
+        ]
+    if any(i for i in schemes):
         return True
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        response, page = r
-        # Anquanbao returns 405 and has reference to /aqb_cc/error/
-        # in its block page
-        if response.status == 405 and b'aqb_cc/error/' in page:
-            return True
     return False
