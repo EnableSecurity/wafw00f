993a2369e26ee4f10396fd3b0403e690822d2fca
0xInfection <theinfecteddrake@gmail.com>
2019-07-18 20:15:21 +0530

    Added aliyundun plugin

--- wafw00f/plugins/aliyundun.py
+++ wafw00f/plugins/aliyundun.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'AliYunDun (Alibaba Cloud Computing)'
 
 
 def is_waf(self):
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        response, page = r
-        if response.status == 405 and b'errors.aliyun.com' in page:
-            return True
+    schemes = [
+        self.matchContent(r'errors.aliyun.com'),
+        self.matchStatus(405)
+        ]
+    if all(i for i in schemes):
+        return True
     return False
