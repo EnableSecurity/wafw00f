efadd0aa1fde127633c271a95fecaed8a334f87b
0xInfection <theinfecteddrake@gmail.com>
2019-07-20 08:37:57 +0530

    Added the anyu plugin

--- wafw00f/plugins/anyu.py
+++ wafw00f/plugins/anyu.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'AnYu (AnYu Technologies)'
 
 
 def is_waf(self):
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        _, page = r
-        if any(i in page for i in (b'Sorry! your access has been intercepted by AnYu', 
-            b'AnYu- the green channel')):
-            return True
+    schemes = [
+        self.matchContent(r'anyu.+?the.green.channel'),
+        self.matchContent(r'sorry.+?your.access.has.been.intercepted.by.anyu')
+        ]
+    if any(i for i in schemes):
+        return True
     return False
