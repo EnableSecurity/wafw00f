4eb183cc2fe3e1e14391fcd80a43332e06fd1046
0xInfection <theinfecteddrake@gmail.com>
2019-07-20 08:46:48 +0530

    Added the blockdos plugin

--- wafw00f/plugins/blockdos.py
+++ wafw00f/plugins/blockdos.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'BlockDoS (BlockDoS)'
 
 
 def is_waf(self):
-    if self.matchheader(('server', 'BlockDos.net')):
+    schemes = [
+        self.matchHeader(('Server', 'blockdos.net'))
+    ]
+    if any(i for i in schemes):
         return True
     return False
