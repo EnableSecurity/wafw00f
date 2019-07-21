42e8ec02cbbbea26f106e0e4d8e4d34f6c3ed4dc
0xInfection <theinfecteddrake@gmail.com>
2019-07-21 17:35:43 +0530

    Added the verber plugin

--- wafw00f/plugins/cerber.py
+++ wafw00f/plugins/cerber.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'WP Cerber Security (Cerber Tech)'
 
 
 def is_waf(self):
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        _, page = r
-        if all(i in page for i in (b'Your request looks suspicious or similar to automated', 
-            b'Our server stopped processing your request', b"We're sorry, you are not allowed to proceed",
-            b'requests from spam posting software', b'<title>403 Access Forbidden</title>')):
-            return True
+    schemes = [
+        self.matchContent(r'your.request.looks.suspicious.or.similar.to.automated'),
+        self.matchContent(r'our.server.stopped.processing.your.request'),
+        self.matchContent(r'We.re.sorry.+you.are.not.allowed.to.proceed'),
+        self.matchContent(r'requests.from.spam.posting.software'),
+        self.matchContent(r'<title>403.Access.Forbidden')
+        ]
+    if all(i for i in schemes):
+        return True
     return False
