c7cbf254f82daad3d5d34fa25cd11d76f7b02319
0xInfection <theinfecteddrake@gmail.com>
2019-07-21 17:41:05 +0530

    Added the denyall plugin

--- wafw00f/plugins/denyall.py
+++ wafw00f/plugins/denyall.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'DenyALL (Rohde & Schwarz CyberSecurity)'
 
 
 def is_waf(self):
-    # Tested against a Rweb 3.8
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        response, _ = r
-        if response.status == 200 and response.reason == 'Condition Intercepted':
-            return True
+    schemes = [
+        self.matchStatus(200),
+        self.matchReason('Condition Intercepted')
+    ]
+    if any(i for i in schemes):
+        return True
     return False
