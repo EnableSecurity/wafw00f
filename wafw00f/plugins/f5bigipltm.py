5f71a197d5014e1d7085d78416bfff54fc8bf4a3
0xInfection <theinfecteddrake@gmail.com>
2019-07-21 17:46:55 +0530

    Added the bigipltm plugin

--- wafw00f/plugins/f5bigipltm.py
+++ wafw00f/plugins/f5bigipltm.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'BIG-IP Local Traffic Manager (F5 Networks)'
 
 
 def is_waf(self):
-    if self.matchcookie(r'^BIGipServer'):
+    schemes = [
+        self.matchCookie('^bigipserver'),
+        self.matchHeader(('X-Cnection', 'close'), attack=True)
+    ]
+    if any(i for i in schemes):
         return True
-    elif self.matchheader(('X-Cnection', r'^close$'), attack=True):
-        return True
-    else:
-        return False
+    return False
