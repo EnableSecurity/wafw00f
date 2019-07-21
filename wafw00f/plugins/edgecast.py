4c4ab9b402aa71a1651d1b9ed36bf4fa80375569
0xInfection <theinfecteddrake@gmail.com>
2019-07-21 17:44:53 +0530

    Added the edgecast plugin

--- wafw00f/plugins/edgecast.py
+++ wafw00f/plugins/edgecast.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'Edgecast (Verizon Digital Media)'
 
 
 def is_waf(self):
-    if self.matchheader(('Server', r'^ECD(.*)?')):
-        return True
-    if self.matchheader(('Server', r'^ECS(.*)?')):
+    schemes = [
+        self.matchHeader(('Server', r'^ECD(.+)?')),
+        self.matchHeader(('Server', r'^ECS(.*)?'))
+    ]
+    if any(i for i in schemes):
         return True
     return False
