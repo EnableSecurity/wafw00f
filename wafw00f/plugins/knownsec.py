28c5eb9a3a24adeffe43308b11d61c32695e412e
0xInfection <theinfecteddrake@gmail.com>
2019-07-23 20:31:22 +0530

    Added knownsec plugin

--- wafw00f/plugins/knownsec.py
+++ wafw00f/plugins/knownsec.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'KS-WAF (KnownSec)'
 
 
 def is_waf(self):
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        _, page = r
-        if b'/ks-waf-error.png' in page:
-            return True
+    schemes = [
+        self.matchContent(r'/ks[-_]waf[-_]error.png')
+    ]
+    if any(i for i in schemes):
+        return True
     return False
