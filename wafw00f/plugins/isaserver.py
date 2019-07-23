9527661528754467032fa11e532aace41efa575d
0xInfection <theinfecteddrake@gmail.com>
2019-07-23 20:29:36 +0530

    Added isasever plugin

--- wafw00f/plugins/isaserver.py
+++ wafw00f/plugins/isaserver.py
@@ -1,24 +1,17 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'ISA Server (Microsoft)'
 
 
 def is_waf(self):
-    isaservermatch = [
-        'Forbidden ( The server denied the specified Uniform Resource Locator (URL). Contact the server administrator.  )',
-        'Forbidden ( The ISA Server denied the specified Uniform Resource Locator (URL)'
+    schemes = [
+        self.matchContent(r'The.+?isa.server.+?denied.the.specified.uniform.resource.locator.\(url\)'),
+        self.matchContent(r'the.server.denied.the.specific.uniform.resource.locator.\(url\).+contact.the.server.administrator')
     ]
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        response, page = r
-        # Not sure whether these long texts are found in response phrases or not :/
-        if response.reason in isaservermatch:
-            return True
-        # This is also found in response page of URLs
-        if any(a in page for a in (b'The ISA Server denied the specified Uniform Resource Locator (URL)',
-            b'The server denied the specified Uniform Resource Locator (URL). Contact the server administrator.')):
-            return True
+    if any(i for i in schemes):
+        return True
     return False
