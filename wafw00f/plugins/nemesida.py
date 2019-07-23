30991aa1c5de6d933b605fc0d7c6f9603fced2ad
0xInfection <theinfecteddrake@gmail.com>
2019-07-23 20:34:02 +0530

    Added nemesida plugin

--- wafw00f/plugins/nemesida.py
+++ wafw00f/plugins/nemesida.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'Nemesida (PentestIt)'
 
 
 def is_waf(self):
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        _, page = r
-        # The WAF requires the owner to create a inbox email ID with alias 'nwaf@site.com' for
-        # reporting anomalies and bugs.
-        if any(i in page for i in (b'Suspicious activity detected. Access to the site is blocked.',  
-            b'nwaf@')):
-            return True
+    schemes = [
+        self.matchContent(r'Suspicious.activity.detected.+?Access.to.the.site.is.blocked.'),
+        self.matchContent(r'nwaf@.+\..+')
+    ]
+    if any(i for i in schemes):
+        return True
     return False
