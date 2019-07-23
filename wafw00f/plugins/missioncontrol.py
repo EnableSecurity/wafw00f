5c7493781e1b5d10fd783a01a754a99af5650f5c
0xInfection <theinfecteddrake@gmail.com>
2019-07-23 20:32:43 +0530

    Added missioncontrol plugin

--- wafw00f/plugins/missioncontrol.py
+++ wafw00f/plugins/missioncontrol.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'Mission Control Application Shield (Mission Control)'
 
 
 def is_waf(self):
-    if self.matchheader(('server', 'Mission Control Application Shield')):
+    schemes = [
+        self.matchHeader(('Server', 'Mission.Control.Application.Shield'))
+    ]
+    if any(i for i in schemes):
         return True
     return False
