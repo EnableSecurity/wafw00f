94f3aaaee72be91818d104629f9568700aeab74b
0xInfection <theinfecteddrake@gmail.com>
2019-07-23 20:30:49 +0530

    Addeed jiasule plugin

--- wafw00f/plugins/jiasule.py
+++ wafw00f/plugins/jiasule.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'Jiasule (Jiasule)'
 
 
 def is_waf(self):
-    # Jiasule has a nice way of expressing itself in server header
-    if self.matchheader(('Server', r'Jiasule\-WAF')):
+    schemes = [
+        self.matchHeader(('Server', r'jiasule\-waf')),
+        self.matchCookie(r'^jsl_tracking(.+)?='),
+        self.matchCookie(r'__jsluid='),
+        self.matchContent(r'notice\-jiasule'),
+        self.matchContent(r'static.jiasule.com')
+    ]
+    if any(i for i in schemes):
         return True
-    # It also expresses itself in cookies
-    if self.matchcookie(r'^jsl_tracking'):
-        return True
-    if self.matchcookie(r'__jsluid='):
-        return True
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        _, page = r
-        # Both are standard fingerprints, so checking for any one works.
-        if any(i in page for i in (b'notice-jiasule', b'static.jiasule.com')):
-            return True
     return False
