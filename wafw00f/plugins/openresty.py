9added3aa6b57cefae93722998e9e4be977b1436
0xInfection <theinfecteddrake@gmail.com>
2019-07-27 11:09:50 +0530

    Added open-resty plugin

--- wafw00f/plugins/openresty.py
+++ wafw00f/plugins/openresty.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'Open-Resty Lua Nginx WAF'
 
 
 def is_waf(self):
-    detected1 = False
-    detected2 = False
-    # Checking if server header is running open-resty
-    if self.matchheader(('Server', r'^openresty/.+?')):
-        detected1 = True
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        response, page = r
-        if response.status == 406 and b'openresty/' in page:
-            return True
-        # Sometimes the blockpage is the default 403 page.
-        if response.status == 403:
-            detected2 = True
-        # If both have returned True, then... we know it is behind the WAF.
-        if detected1 and detected2:
-            return True
+    schema1 = [
+        self.matchHeader(('Server', r'^openresty/[0-9\.]+?')),
+        self.matchStatus(403)
+    ]
+    schema2 = [
+        self.matchContent(r'openresty/[0-9\.]+?'),
+        self.matchStatus(406)
+    ]
+    if all(i for i in schema1):
+        return True
+    if all(i for i in schema2):
+        return True
     return False
