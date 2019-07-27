617b3f80984166b36111529e774953ddc1abe65c
0xInfection <theinfecteddrake@gmail.com>
2019-07-27 11:09:09 +0530

    Added nsfocus plugin

--- wafw00f/plugins/nsfocus.py
+++ wafw00f/plugins/nsfocus.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'NSFocus (NSFocus Global Inc.)'
 
 
 def is_waf(self):
-    if self.matchheader(('server', 'NSFocus')):
+    schemes = [
+        self.matchHeader(('Server', 'NSFocus'))
+    ]
+    if any(i for i in schemes):
         return True
     return False
