75687e0c270bb7b3e747b1a9679e7946cf481fa0
0xInfection <theinfecteddrake@gmail.com>
2019-07-27 11:10:05 +0530

    Added palo alto plugin

--- wafw00f/plugins/paloalto.py
+++ wafw00f/plugins/paloalto.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'Palo Alto Next Gen Firewall (Palo Alto Networks)'
 
 
 def is_waf(self):
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        _, page = r
-        if all(i in page for i in (b'Download of virus/spyware blocked', b'blocked in accordance with company policy')):
-            return True
-        if b'Palo Alto Next Generation Security Platform' in page:
-            return True
+    schemes = [
+        self.matchContent(r'Download.of.virus.spyware.blocked'),
+        self.matchContent(r'Palo.Alto.Next.Generation.Security.Platform')
+    ]
+    if any(i for i in schemes):
+        return True
     return False
