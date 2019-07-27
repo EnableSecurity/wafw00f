87b73a3bc409792298d67ce7a21239ff09c25bbd
0xInfection <theinfecteddrake@gmail.com>
2019-07-27 11:08:54 +0530

    Added ninja plugin

--- wafw00f/plugins/ninja.py
+++ wafw00f/plugins/ninja.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'NinjaFirewall (NinTechNet)'
 
 
 def is_waf(self):
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        _, page = r
-        # Sites without modified block page return both, so checking for all
-        # of them works.
-        if all(i in page for i in (b'For security reasons, it was blocked and logged',
-            b'<title>NinjaFirewall: 403 Forbidden')):
-            return True
+    schemes = [
+        self.matchContent(r'<title>NinjaFirewall.+?\d{3}.forbidden'),
+        self.matchContent(r'For.security.reasons?.+?it.was.blocked.and.logged')
+    ]
+    if any(i for i in schemes):
+        return True
     return False
