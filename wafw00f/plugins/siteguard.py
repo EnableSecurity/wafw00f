0f0a9c2c9ef158370f83aa44883796f35dd593a5
0xInfection <theinfecteddrake@gmail.com>
2019-08-12 17:27:25 +0530

    Added siteguard plugin

--- wafw00f/plugins/siteguard.py
+++ wafw00f/plugins/siteguard.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'SiteGuard (Sakura Inc.)'
 
 
 def is_waf(self):
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        _, page = r
-        if any(i in page for i in (b'Powered by SiteGuard', b'The server refuse to browse the page')):
-            return True
+    schemes = [
+        self.matchContent(r"Powered.by.SiteGuard"),
+        self.matchContent(r'The.server.refuse.to.browse.the.page')
+    ]
+    if any(i for i in schemes):
+        return True
     return False
