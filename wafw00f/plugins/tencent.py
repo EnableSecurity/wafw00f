1fcecca3f41b4a47b55359d6f8105f92824705dc
0xInfection <theinfecteddrake@gmail.com>
2019-08-13 11:43:54 +0530

    Added tencent plugin

--- wafw00f/plugins/tencent.py
+++ wafw00f/plugins/tencent.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'Tencent Cloud Firewall (Tencent Technologies)'
 
 
 def is_waf(self):
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        _, page = r
-        if b'waf.tencent-cloud.com/' in page:
-            return True
+    schemes = [
+        self.matchContent(r'waf.tencent\-?cloud.com/')
+    ]
+    if any(i for i in schemes):
+        return True
     return False
