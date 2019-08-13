30890f3a94f9cedfffe6b5c31332c70ee96ca0fd
0xInfection <theinfecteddrake@gmail.com>
2019-08-13 11:44:07 +0530

    Added teros plugin

--- wafw00f/plugins/teros.py
+++ wafw00f/plugins/teros.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'Teros (Citrix Systems)'
 
 
 def is_waf(self):
-    if self.matchcookie(r'^st8id='):
+    schemes = [
+        self.matchCookie(r'^st8id=')
+    ]
+    if any(i for i in schemes):
         return True
     return False
