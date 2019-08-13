df74f62c06c1e13840522540a7bfc6094d2eebb6
0xInfection <theinfecteddrake@gmail.com>
2019-08-13 11:42:38 +0530

    Added sophos plugin

--- wafw00f/plugins/sophos.py
+++ wafw00f/plugins/sophos.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
-# Well this is confusing, Sitelock itself uses Incapsula from Imperva
-# So the fingerprints obtained on blockpage are exactly similar to those of Incapsula.
 NAME = 'UTM Web Protection (Sophos)'
 
 
 def is_waf(self):
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        _, page = r
-        # These fingerprints are the ones found in UTM 9 overall, block with debug mode
-        # enabled as well as disabled.
-        if any(i in page for i in (b'http://www.sophos.com', b'UTM')):
-            return True
-        # These are fingerprints when the debug mode is not enabled
-        if any(i in page for i in (b'Powered by Sophos', b'Powered by UTM Web Protection')):
-            return True
-        # These fingerprints are those when debug mode is enabled in the WAF
-        # So many fingerprints for narrowing down the detecting stuff
-        if all(i in page for i in (b'<title>Access to the requested URL was blocked', 
-            b'Access to the requested URL was blocked', b'incident was logged with the following log identifier', b'Inbound Anomaly Score Exceeded', b'Your cache administrator is')):
-            return True
+    schema1 = [
+        self.matchContent(r'www.sophos.com'),
+        self.matchContent(r'Powered.by.?(Sophos)?.UTM.Web.Protection')
+    ]
+    schema2 = [
+        self.matchContent(r'<title>Access.to.the.requested.URL.was.blocked'),
+        self.matchContent(r'Access.to.the.requested.URL.was.blocked'),
+        self.matchContent(r'incident.was.logged.with.the.following.log.identifier'),
+        self.matchContent(r'Inbound.Anomaly.Score.exceeded'),
+        self.matchContent(r'Your.cache.administrator.is')
+    ]
+    if any(i for i in schema1):
+        return True
+    if all(i for i in schema2):
+        return True
     return False
