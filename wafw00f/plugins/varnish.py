124d491a67ade1764da26669d7a9238b7d4c9def
0xInfection <theinfecteddrake@gmail.com>
2019-08-15 12:58:21 +0530

    Added varnish plugin

--- wafw00f/plugins/varnish.py
+++ wafw00f/plugins/varnish.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'Varnish (OWASP)'
 
 
 def is_waf(self):
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        _, page = r
-        # This often displayed when the debug mode is enabled.
-        # Otherwise it is the regular 403vblockpage itself.
-        if b'Request rejected by xVarnish-WAF' in page:
-            return True
+    schemes = [
+        self.matchContent(r'Request.rejected.by.xVarnish\-WAF')
+    ]
+    if any(i for i in schemes):
+        return True
     return False
