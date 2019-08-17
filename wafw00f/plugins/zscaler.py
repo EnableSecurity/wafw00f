909cfb8f3c8143571da0e8ed0bf266fbb13ebb10
0xInfection <theinfecteddrake@gmail.com>
2019-08-17 10:32:22 +0530

    Added zscaler plugin

--- wafw00f/plugins/zscaler.py
+++ wafw00f/plugins/zscaler.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'ZScaler (Accenture)'
 
 
 def is_waf(self):
-    # ZScaler has its own server header set
-    if self.matchheader(('Server', 'ZScaler')):
+    schemes = [
+        self.matchHeader(('Server', r'ZScaler')),
+        self.matchContent(r"Access.Denied.+?Accenture.Policy"),
+        self.matchContent(r'policies\.accenture\.com'),
+        self.matchContent(r'login\.zscloud\.net/img_logo_new1\.png'),
+        self.matchContent(r'Zscaler.to.protect.you.from.internet.threats'),
+        self.matchContent(r"Accenture.+?webfilters.indicate.that.the.site.likely.contains")
+    ]
+    if any(i for i in schemes):
         return True
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        _, page = r
-        # ZScaler has lot of fingerprints in its blockpage, here are some
-        if any(i in page for  i in (b'Access Denied: Accenture Policy', b'policies.accenture.com',
-            b'login.zscloud.net/img_logo_new1.png', b'Zscaler to protect you from internet threats',
-            b"Accenture's webfilters indicate that the site likely contains content considered inappropriate")):
-            return True
     return False
