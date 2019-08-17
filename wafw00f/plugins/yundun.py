51f6d4f82e912063f931a1876f54331ce6bed0b0
0xInfection <theinfecteddrake@gmail.com>
2019-08-17 10:31:19 +0530

    Added yundun plugin

--- wafw00f/plugins/yundun.py
+++ wafw00f/plugins/yundun.py
@@ -1,27 +1,21 @@
 #!/usr/bin/env python
-
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
 
 NAME = 'Yundun (Yundun)'
 
 
 def is_waf(self):
-    # Yundun has its own server header set
-    if self.matchheader(('Server', 'YUNDUN')):
+    schemes = [
+        self.matchHeader(('Server', 'YUNDUN')),
+        self.matchHeader(('X-Cache', 'YUNDUN')),
+        self.matchCookie(r'^yd_cookie='),
+        self.matchContent(r'Blocked.by.YUNDUN.Cloud.WAF'),
+        self.matchContent(r'yundun\.com/yd[-_]http[_-]error/'),
+        self.matchContent(r'www\.yundun\.com/(static/js/fingerprint\d{1}?\.js)?')
+    ]
+    if any(i for i in schemes):
         return True
-    #  X-Cache too sometimes
-    if self.matchheader(('X-Cache', 'YUNDUN')):
-        return True
-    # Found more fingerprints for Yundun during testing phase
-    if self.matchcookie(r'^yd_cookie='):
-        return True
-    for attack in self.attacks:
-        r = attack(self)
-        if r is None:
-            return
-        _, page = r
-        # Yundun has its nice characteristic blockpage. Found this new unique fingerprint
-        # while in testing phase
-        if any(i in page for  i in (b'Blocked by YUNDUN Cloud WAF', b'yundun.com/yd_http_error/',
-            b'www.yundun.com/static/js/fingerprint2.js')):
-            return True
     return False
