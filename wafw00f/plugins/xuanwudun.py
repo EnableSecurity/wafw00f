dcab6ec49b0f9eb95fb3745998db19ebe7f3ef9c
0xInfection <theinfecteddrake@gmail.com>
2019-08-17 10:31:04 +0530

    Another fingerprint for zuanwudun

--- wafw00f/plugins/xuanwudun.py
+++ wafw00f/plugins/xuanwudun.py
@@ -9,7 +9,8 @@ NAME = 'Xuanwudun'
 
 def is_waf(self):
     schemes = [
-        self.matchContent(r"admin\.dbappwaf\.cn/index\.php/Admin/ClientMisinform/")
+        self.matchContent(r"admin\.dbappwaf\.cn/(index\.php/Admin/ClientMisinform/)?"),
+        self.matchContent(r'<.+class=.+?(db[\-_]?)?waf(.)?([\-_]?row)?>')
     ]
     if any(i for i in schemes):
         return True
