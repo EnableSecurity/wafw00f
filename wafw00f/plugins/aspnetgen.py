b1ab9f26015889dfa67e93dbc742dde826de5e9d
0xInfection <theinfecteddrake@gmail.com>
2019-08-23 11:41:47 +0530

    Added fix to regex error

--- wafw00f/plugins/aspnetgen.py
+++ wafw00f/plugins/aspnetgen.py
@@ -9,7 +9,7 @@ NAME = 'ASP.NET Generic Protection (Microsoft)'
 
 def is_waf(self):
     schemes = [
-        self.matchContent(r'iis.(\d?+.)+?detailed.error'),
+        self.matchContent(r'iis.(\d+.)+?detailed.error'),
         self.matchContent(r'potentially.dangerous.request.querystring'),
         self.matchContent(r'application.error.from.being.viewed.remotely.(for.security.reasons)?'),
         self.matchContent(r'An.application.error.occurred.on.the.server'),
