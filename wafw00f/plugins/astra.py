867f88f6c55f1358f4ac70b8260b3648fc938fec
0xInfection <theinfecteddrake@gmail.com>
2019-10-01 23:02:14 +0530

    Fingerprint update for astra

--- wafw00f/plugins/astra.py
+++ wafw00f/plugins/astra.py
@@ -10,6 +10,7 @@ NAME = 'Astra Web Protection (Czar Securities)'
 def is_waf(self):
     schemes = [
         self.matchCookie(r'^cz_astra_csrf_cookie'),
+        self.matchContent(r'astrawebsecurity\.freshdesk\.com'),
         self.matchContent(r'www.getastra.com/assets/images')
     ]
     if any(i for i in schemes):
