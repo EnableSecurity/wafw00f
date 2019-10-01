a6f1f71d34581a96e59047e6f8e3c8a11b319ac2
0xInfection <theinfecteddrake@gmail.com>
2019-10-01 23:48:48 +0530

    Added followup for astra

--- wafw00f/plugins/astra.py
+++ wafw00f/plugins/astra.py
@@ -11,7 +11,7 @@ def is_waf(self):
     schemes = [
         self.matchCookie(r'^cz_astra_csrf_cookie'),
         self.matchContent(r'astrawebsecurity\.freshdesk\.com'),
-        self.matchContent(r'www.getastra.com/assets/images')
+        self.matchContent(r'www\.getastra\.com/assets/images')
     ]
     if any(i for i in schemes):
         return True
