9ff56710509bee9dd906f82ed0b4af43e09293f7
0xInfection <theinfecteddrake@gmail.com>
2019-10-02 21:17:33 +0530

    Added viettel waf

---
+++ wafw00f/plugins/viettel.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
+
+NAME = 'Viettel (Cloudrity)'
+
+
+def is_waf(self):
+    schemes = [
+        self.matchContent(r"Access.Denied.+?Viettel.WAF"),
+        self.matchContent(r"cloudrity\.com\.(vn)?/"),
+        self.matchContent(r"Viettel.WAF.System")
+    ]
+    if any(i for i in schemes):
+        return True
+    return False
