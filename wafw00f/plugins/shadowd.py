7a9dc07e133cba7d5070885827f509d5032d7ca8
0xInfection <theinfecteddrake@gmail.com>
2019-10-02 21:18:49 +0530

    Added shadowd waf plugin

---
+++ wafw00f/plugins/shadowd.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
+
+NAME = 'Shadow Daemon (Zecure)'
+
+
+def is_waf(self):
+    schemes = [
+        self.matchContent(r"<h\d{1}>\d{3}.forbidden<.h\d{1}>"),
+        self.matchContent(r"request.forbidden.by.administrative.rules")
+    ]
+    if all(i for i in schemes):
+        return True
+    return False
