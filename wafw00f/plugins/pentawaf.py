e0dc425d832c5a5575cff09d5e40bb3544bf353d
0xInfection <theinfecteddrake@gmail.com>
2019-10-02 21:47:58 +0530

    Added pentawaf plugin

---
+++ wafw00f/plugins/pentawaf.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
+
+NAME = 'PentaWAF (Global Network Services)'
+
+
+def is_waf(self):
+    schemes = [
+        self.matchHeader(('Server', r'PentaWaf(/[0-9\.]+)?')),
+        self.matchContent(r'Penta.?Waf/[0-9\.]+?.server')
+    ]
+    if any(i for i in schemes):
+        return True
+    return False
