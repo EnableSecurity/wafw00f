caa08ae4c92bb46ad7260cd6ea7681b13faebd8b
0xInfection <theinfecteddrake@gmail.com>
2019-10-04 09:14:25 +0530

    Update for apptrana

--- wafw00f/plugins/apptrana.py
+++ wafw00f/plugins/apptrana.py
@@ -10,7 +10,7 @@ NAME = 'AppTrana (Indusface)'
 def is_waf(self):
     schemes = [
         self.matchHeader(('Server', r'IF_WAF')),
-        self.matchContent(r'This website is secured against online attacks. Your request was blocked\w+'),
+        self.matchContent(r'This website is secured against online attacks. Your request was blocked'),
         self.matchContent(r'(?s)Client.IP.+?Incident.Time.+?Incident.ID')
     ]
     if any(i for i in schemes):
