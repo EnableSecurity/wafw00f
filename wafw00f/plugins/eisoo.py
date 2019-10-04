e715b2679b6c2b056c27df401bb6ff2599222603
0xInfection <theinfecteddrake@gmail.com>
2019-10-04 09:15:48 +0530

    Added eisoo waf

---
+++ wafw00f/plugins/eisoo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
+
+NAME = 'Eisoo Cloud Firewall (Eisoo)'
+
+
+def is_waf(self):
+    schemes = [
+        self.matchHeader(('Server', r'EisooWAF(\-AZURE)?/?')),
+        self.matchContent(r'<link.+href=\"/eisoo\-firewall\-block\.css'),
+        self.matchContent(r'www\.eisoo\.com'),
+        self.matchContent(r'&copy;.\d{4}.Eisoo.Inc.')
+    ]
+    if any(i for i in schemes):
+        return True
+    return False
