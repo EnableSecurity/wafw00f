9db7236034c311bd84872eda6570cb818a158621
0xInfection <theinfecteddrake@gmail.com>
2019-10-04 09:50:05 +0530

    Added safe3 firewall

--- wafw00f/plugins/safe3.py
+++ wafw00f/plugins/safe3.py
@@ -10,6 +10,7 @@ NAME = 'Safe3 Web Firewall (Safe3)'
 def is_waf(self):
     schemes = [
         self.matchHeader(('Server', 'Safe3 Web Firewall')),
+        self.matchHeader(('X-Powered-By', r'Safe3WAF/[\.0-9]+?')),
         self.matchContent(r'Safe3waf/[0-9\.]+?')
     ]
     if any(i for i in schemes):
