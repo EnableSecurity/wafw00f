cf69427ac039a8d11a15c08b46f915fb3cefa21c
0xInfection <theinfecteddrake@gmail.com>
2019-10-04 10:38:27 +0530

    Added NullDDOS waf plugin

---
+++ wafw00f/plugins/nullddos.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
+
+NAME = 'NullDDoS Protection (NullDDoS)'
+
+
+def is_waf(self):
+    schemes = [
+        self.matchHeader(('Server', r'NullDDoS(.System)?'))
+    ]
+    if any(i for i in schemes):
+        return True
+    return False
