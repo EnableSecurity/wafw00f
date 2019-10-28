a11cfdc8b3a00dcb161cf3119a0dfa61ba86f247
0xInfection <theinfecteddrake@gmail.com>
2019-10-28 13:35:03 +0530

    Added more fingerprints for aliyundun

--- wafw00f/plugins/aliyundun.py
+++ wafw00f/plugins/aliyundun.py
@@ -10,6 +10,8 @@ NAME = 'AliYunDun (Alibaba Cloud Computing)'
 def is_waf(self):
     schemes = [
         self.matchContent(r'error(s)?\.aliyun(dun)?\.(com|net)?'),
+        self.matchCookie(r'^aliyungf_tc='),
+        self.matchContent(r'cdn\.aliyun(cs)?\.com'),
         self.matchStatus(405)
         ]
     if all(i for i in schemes):
