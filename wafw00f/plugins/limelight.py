bc0b6fe7390db373d6434e9a2233176cc7554018
0xInfection <theinfecteddrake@gmail.com>
2019-10-28 13:09:28 +0530

    Adding limelight cdn waf

---
+++ wafw00f/plugins/limelight.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+'''
+Copyright (C) 2019, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
+
+NAME = 'LimeLight CDN (LimeLight)'
+
+
+def is_waf(self):
+    schemes = [
+        self.matchCookie(r'^limelight'),
+        self.matchCookie(r'^l[mg]_sessid=')
+    ]
+    if any(i for i in schemes):
+        return True
+    return False
