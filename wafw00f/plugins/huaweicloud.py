de665d2917b683c085d59edc059a3d62f54ac919
0xInfection <theinfecteddrake@gmail.com>
2019-10-28 13:08:44 +0530

    Adding the huaweicloud waf

--- wafw00f/plugins/huaweicloud.py
+++ wafw00f/plugins/huaweicloud.py
@@ -9,6 +9,8 @@ NAME = 'Huawei Cloud Firewall (Huawei)'
 
 def is_waf(self):
     schemes = [
+        self.matchCookie(r'^HWWAFSESID='),
+        self.matchHeader(('Server', r'HuaweiCloudWAF')),
         self.matchContent(r'hwclouds\.com'),
         self.matchContent(r'hws_security@')
     ]
