c501c6fdd17c4c5cddbc9633251416f687be5613
root <root@Infected-Machine.localdomain>
2019-12-06 19:32:18 +0530

    Header update for browser emulation

--- wafw00f/lib/evillib.py
+++ wafw00f/lib/evillib.py
@@ -23,13 +23,12 @@ except ImportError:
 # For requests >= 2.16, this is the convention 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def_headers = {'User-Agent'     : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3770.100 Safari/537.36',
-               'Accept'         : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+def_headers = {'Accept'         : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
                'Accept-Encoding': 'gzip, deflate',
                'Accept-Language': 'en-US,en;q=0.9',
                'DNT'            : '1',  # Do Not Track request header
-               'Cache-Control'  : 'max-age=0',
-               'Connection'     : 'keep-alive'
+               'User-Agent'     : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3770.100 Safari/537.36',
+               'Upgrade-Insecure-Requests': '1' #
         }
 proxies = {}
 
