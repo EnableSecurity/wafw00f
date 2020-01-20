122f380e9cbde44d64875b3b35b4a03497af8c40
Sandro Gauci <sandro@enablesecurity.com>
2020-01-20 16:09:58 +0100

    fixed unhandled exception in del self.headers['User-Agent']

--- wafw00f/main.py
+++ wafw00f/main.py
@@ -84,7 +84,8 @@ class WAFW00F(waftoolsengine):
         try:
             # Testing for no user-agent response. Detects almost all WAFs out there.
             resp1 = self.performCheck(self.normalRequest)
-            del self.headers['User-Agent']  # Deleting the user-agent key from object not dict.
+            if 'User-Agent' in self.headers:
+                del self.headers['User-Agent']  # Deleting the user-agent key from object not dict.
             resp3 = self.customRequest(headers=def_headers)
             if resp1.status_code != resp3.status_code:
                 self.log.info('Server returned a different response when request didn\'t contain the User-Agent header.')
