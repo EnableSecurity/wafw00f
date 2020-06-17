602c487577b24d850269c3f47ee81bcaf2cdbaff
Pinaki Mondal <theinfecteddrake@gmail.com>
2020-06-17 12:40:47 +0530

    Added another schema to the plugin

--- wafw00f/plugins/wpmudev.py
+++ wafw00f/plugins/wpmudev.py
@@ -8,12 +8,20 @@ NAME = 'wpmudev WAF (Incsub)'
 
 
 def is_waf(self):
-    schemes = [
-        self.matchContent(r'Log in to the <a href="https://wpmudev.com/hub/hosting/" rel="noreferrer">Hosting Hub</a>'),
-        self.matchContent(r'Choose your site from the list'),
+    schema1 = [
+        self.matchContent(r'href="http(s)?.\/\/wpmudev.com\/.{0,15}?'),
         self.matchContent(r'Click on the Logs tab, then the WAF Log.'),
+        self.matchContent(r'Choose your site from the list'),
         self.matchStatus(403)
     ]
-    if all(i for i in schemes):
+    schema2 = [
+        self.matchContent(r'<h1>Whoops, this request has been blocked!'),
+        self.matchContent(r'This request has been deemed suspicious'),
+        self.matchContent(r'possible attack on our servers.'),
+        self.matchStatus(403)
+    ]
+    if all(i for i in schema1):
+        return True
+    if all(i for i in schema2):
         return True
     return False
