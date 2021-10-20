0a94302cc7a8c6c9d395b296c848f7c93c495e17
foozzi <foozzione@gmail.com>
2021-10-20 14:19:21 +0300

    add ddog-guard

---
+++ wafw00f/plugins/ddosguard.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+'''
+Copyright (C) 2020, WAFW00F Developers.
+See the LICENSE file for copying permission.
+'''
+
+NAME = 'Ddos-guard (DDOS-GUARD CORP.)'
+
+
+def is_waf(self):
+    schemes = [
+        self.matchCookie(r'^__ddg1.*?='),
+        self.matchCookie(r'^__ddg2.*?='),
+        self.matchCookie(r'^__ddgid.*?='),
+        self.matchCookie(r'^__ddgmark.*?='),
+        self.matchHeader(('Server', 'ddos-guard')),
+    ]
+    if any(i for i in schemes):
+        return True
+    return False
