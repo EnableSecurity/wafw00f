e3abd4910ebf883e590a1115a2c3bb917d302a8a
Pinaki Mondal <theinfecteddrake@gmail.com>
2023-06-04 11:46:49 +0530

    updated variti waf signatures

--- wafw00f/plugins/variti.py
+++ wafw00f/plugins/variti.py
@@ -6,9 +6,8 @@ See the LICENSE file for copying permission.
 
 NAME = 'Variti (Variti)'
 
-
 def is_waf(self):
-    if self.matchHeader(('Server', r'VARITI')):
+    if self.matchHeader(('Server', r'Variti(?:\/[a-z0-9\.\-]+)?')):
         return True
 
     return False
