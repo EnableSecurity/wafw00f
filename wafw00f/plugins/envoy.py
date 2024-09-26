afa3dbc6fec7c596d4442dacfdaaf52725f28409
0xInfection <0xinfection@gmail.com>
2024-09-26 19:30:19 +0530

    added all envoy detection headers

--- wafw00f/plugins/envoy.py
+++ wafw00f/plugins/envoy.py
@@ -11,4 +11,31 @@ def is_waf(self):
     if self.matchHeader(('server', 'envoy')):
         return True
 
+    if self.matchHeader(('x-envoy-upstream-service-time', '.+')):
+        return True
+
+    if self.matchHeader(('x-envoy-downstream-service-cluster', '.+')):
+        return True
+
+    if self.matchHeader(('x-envoy-downstream-service-node', '.+')):
+        return True
+
+    if self.matchHeader(('x-envoy-external-address', '.+')):
+        return True
+
+    if self.matchHeader(('x-envoy-force-trace', '.+')):
+        return True
+
+    if self.matchHeader(('x-envoy-internal', '.+')):
+        return True
+
+    if self.matchHeader(('x-envoy-original-dst-host', '.+')):
+        return True
+
+    if self.matchHeader(('x-envoy-original-path', '.+')):
+        return True
+
+    if self.matchHeader(('x-envoy-local-overloaded', '.+')):
+        return True
+
     return False
